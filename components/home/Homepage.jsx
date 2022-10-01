009e1714fa85eabdf88449f5f8c0942bb2f9e48a
Navinda Chandrasiri <adilanga@gmail.com>
2022-10-01 15:07:59 +0530

    Homepage Updated

--- components/home/Homepage.jsx
+++ components/home/Homepage.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Link from "next/link";
+import UserService from "../../services/UserService";
 
 function Homepage(props) {
+    const [currentUser, setCurrentUser] = useState();
+    const [adminUser, setAdminUser] = useState();
+    const [defaultUser, setDefaultUser] = useState();
+    const [buyerUser, setBuyerUser] = useState();
+    const [sellerUser, setSellerUser] = useState();
+
+    useEffect(() => {
+        async function dataFetch() {
+            const temp = UserService.getCurrentUser();
+
+            if (temp) {
+                setCurrentUser(temp);
+                setAdminUser(temp.roles.includes("ROLE_ADMIN"));
+                setDefaultUser(temp.roles.includes("ROLE_DEFAULT"));
+                setBuyerUser(temp.roles.includes("ROLE_BUYER"));
+                setSellerUser(temp.roles.includes("ROLE_SELLER"));
+            }
+            console.log(temp);
+        }
+
+        dataFetch();
+    }, []);
+
     return (
         <div className='w-full h-screen text-center'>
             <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
@@ -16,18 +40,38 @@ function Homepage(props) {
                     </div>
 
                     <div className='col-span-2 lg:col-span-2 w-full h-auto p-4'>
-                        <div>
-                            <div>
-                                <Link href='/login'>
-                                    <button className='max-w-[200px] w-full mt-6 bg-indigo-600 rounded-lg px-4 py-2 text-lg text-white tracking-wide font-semibold font-sans'>Sign In</button>
-                                </Link>
+                        {currentUser ? (
+                            <div className="navbar-nav ml-auto">
+                                {buyerUser && (
+                                    <div>
+                                        <Link href='/'>
+                                            <button className='max-w-[200px] w-full mt-6 bg-indigo-600 rounded-lg px-4 py-2 text-lg text-white tracking-wide font-semibold font-sans'>Documentation</button>
+                                        </Link>
+                                    </div>
+                                )}
+                                {sellerUser && (
+                                    <div>
+                                        <Link href='/'>
+                                            <button className='max-w-[200px] w-full mt-6 bg-indigo-600 rounded-lg px-4 py-2 text-lg text-white tracking-wide font-semibold font-sans'>Contact Us</button>
+                                        </Link>
+                                    </div>
+                                )}
+                                <h1></h1>
                             </div>
+                        ) : (
                             <div>
-                                <Link href='/registration'>
-                                    <button className='max-w-[200px] w-full mt-6 bg-indigo-600 rounded-lg px-4 py-2 text-lg text-white tracking-wide font-semibold font-sans'>Sign Up</button>
-                                </Link>
+                                <div>
+                                    <Link href='/login'>
+                                        <button className='max-w-[200px] w-full mt-6 bg-indigo-600 rounded-lg px-4 py-2 text-lg text-white tracking-wide font-semibold font-sans'>Sign In</button>
+                                    </Link>
+                                </div>
+                                <div>
+                                    <Link href='/registration'>
+                                        <button className='max-w-[200px] w-full mt-6 bg-indigo-600 rounded-lg px-4 py-2 text-lg text-white tracking-wide font-semibold font-sans'>Sign Up</button>
+                                    </Link>
+                                </div>
                             </div>
-                        </div>
+                        )}
                     </div>
                 </div>
             </div>
